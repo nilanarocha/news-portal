a66f26583a90e8ac3ae72fab6eb2a101780bd870
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-09-05 21:46:44 +1000

    adding support for search by author and category

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import BaseLayout from "./BaseLayout";
+import queryString from "query-string";
 import axios from "axios";
 
 class SearchPage extends Component {
@@ -10,7 +11,19 @@ class SearchPage extends Component {
 
   async componentDidMount() {
     try {
-      const response = await axios.get("http://localhost:3000/api/search");
+      const search = [];
+      const values = queryString.parse(this.props.location.search);
+
+      if (values.search) {
+        search.push(`search=${values.search}`);
+      }
+      if (values.news_category) {
+        search.push(`news_category=${values.news_category}`);
+      }
+
+      const response = await axios.get(
+        `http://localhost:3000/api/search?${search.join("&")}`
+      );
       this.setState({
         search: response.data
       });
@@ -23,24 +36,25 @@ class SearchPage extends Component {
   render() {
     const { search } = this.state;
 
-    if (search.length === 0) {
-      return null;
-    }
     return (
       <BaseLayout>
         <div>
           <h1>{search.name}</h1>
-          {search.map(item => {
-            return (
-              <Link key={item.id} to={`/news/${item.id}`}>
-                <div>
-                  <img src={item.image} alt="author" width="100" />
-                  <h3>{item.title}</h3>
-                  <div>{item.description}</div>
-                </div>
-              </Link>
-            );
-          })}
+          {search.length > 0 ? (
+            search.map(item => {
+              return (
+                <Link key={item.id} to={`/news/${item.id}`}>
+                  <div>
+                    <img src={item.image} alt="author" width="100" />
+                    <h3>{item.title}</h3>
+                    <div>{item.description}</div>
+                  </div>
+                </Link>
+              );
+            })
+          ) : (
+            <h3>No results</h3>
+          )}
         </div>
       </BaseLayout>
     );
--- server/app/controllers/api_news_controller.rb
+++ server/app/controllers/api_news_controller.rb
@@ -32,16 +32,55 @@ class ApiNewsController < ApplicationController
     render json: @author
   end
 
-  def get_search
-    @news = News.all.order('created_at desc')
-    render json: @news
-  end
-
   def get_news_category
     limit = nil
     limit = params[:limit] if params[:limit]
 
     @news_category = NewsCategory.all.order('created_at desc').limit(limit)
     render json: @news_category
+  end
+
+  def get_search
+    # Showing the text used for search
+    @search = params[:search]
+    @news_category = params[:news_category]
+    sql_complement = []
+    # Checking if search was informed via search form
+    # if informed, should add to the query
+    # .blank? checking if the variable exist
+    unless @search.blank?
+      # Checking if we have authors for the search
+      @authors = Author.where("LOWER(name) LIKE LOWER('%#{@search}%')")
+                       .pluck(:id)
+
+      #  If we have authors, the search checks for all the news by author
+      sql_authors_complement = if @authors.any?
+                                 "OR authors_id IN (#{@authors.join(', ')})"
+                               else
+                                 ''
+                               end
+      # searching case insensitive "LOWER(title) LIKE LOWER('%#{@search}%')"
+      sql_complement.push(
+        "
+  (LOWER(title) LIKE LOWER('%#{@search}%')
+  OR LOWER(description) LIKE LOWER('%#{@search}%')
+  OR LOWER(headline) LIKE LOWER('%#{@search}%')
+  #{sql_authors_complement}
+  )
+  "
+      )
+    end
+
+    # Checking if news_category was informed via search form
+    # if informed, should add to the query
+    if !@news_category.blank? && @news_category != 'all'
+      # seaching only for the specified property type
+      sql_complement.push("news_categories_id=#{NewsCategory.find_by(name: @news_category).id}")
+    end
+
+    # Search all properties with the search title
+    @news = News.where(sql_complement.join(' AND ').to_s).order('created_at desc')
+
+    render json: @news
   end
 end
